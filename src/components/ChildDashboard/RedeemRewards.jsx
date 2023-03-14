45e29a172e82d78359a9426f29e795ce15195d88
daryacoding <abdullahzadeh.darya@gmail.com>
2023-03-14 11:57:38 -0400

    redeem rewards component created

--- src/components/ChildDashboard/RedeemRewards.jsx
+++ src/components/ChildDashboard/RedeemRewards.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function RedeemRewards() {
+    const [rewards, setRewards] = useState([])
+    const listRewards = async () => {
+        try {
+            const response = await fetch('/reward', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const data = await response.json()
+            setRewards(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        listRewards()
+    }, [rewards])
+
     return (
-        <div>RedeemRewards</div>
+        <div className='redeem-rewards'>
+            <h4>Redeem Rewards</h4>
+            <div className='total-points'>Total Points</div>
+            return(
+                <ul>
+                    {rewards.map((reward) => {
+                        return (
+                            <li key={reward._id}>
+                                <div className='reward-name'>{reward.rewardName}</div>
+                                <div className='reward-points'>{reward.rewardPoints} Points</div>
+                                <button>Buy</button>
+                            </li>
+                        )
+                    })}
+                </ul>
+                )
+        </div>
     )
 }
