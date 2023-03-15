86c5eb562f7c5835693065a3cdc57f9e56ef4693
EmilyJarecki <jareckiemily@yahoo.com>
2023-03-15 12:55:20 -0500

    accordian is working

--- src/components/CaregiverDashboard/ShowAndEditTask.jsx
+++ src/components/CaregiverDashboard/ShowAndEditTask.jsx
@@ -5,111 +5,131 @@ import * as Yup from "yup";
 import axios from "axios";
 
 const ShowAndEditTask = ({ taskIds }) => {
-    const navigate = useNavigate();
-    const [tasks, setTasks] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(false); // track whether API call is complete
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchTasks = async () => {
-            const taskData = await Promise.all(
-                taskIds.map((taskId) => axios.get(`http://localhost:4000/tasks/${taskId}`))
-            );
-            const taskList = taskData.map((task, index) => ({
-                id: taskIds[index],
-                taskName: task.data.taskName || "",
-                taskDescription: task.data.taskDescription || "",
-                completed: task.data.completed || false,
-                image: "",
-                taskPoints: task.data.taskPoints || "",
-                dueDate: "",
-            }));
-            setTasks(taskList);
-            setIsLoaded(true); // update state to indicate API call is complete
-        };
-        fetchTasks();
-    }, [taskIds]);
+  //accordian
+  const [openTaskId, setOpenTaskId] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false); // track whether API call is complete
 
-    const validationSchema = Yup.object().shape({
-        taskName: Yup.string().required("Required"),
-        taskDescription: Yup.string().required("Required"),
-        taskPoints: Yup.number()
-            .typeError("Must be a number")
-            .positive("Must be positive")
-            .required("Required"),
-        dueDate: Yup.date().required("Required"),
-    });
+  const handleOpen = (index) => {
+    setOpenTaskId(index);
+  };
 
-    const handleSubmit = (values, taskId) => {
-        axios
-            .put(`http://localhost:4000/tasks/${taskId}`, values)
-            .then(() => {
-                console.log("Task updated successfully");
-                navigate("/tasksrewards");
-            })
-            .catch((err) => console.log(err));
+  const handleClose = () => {
+    setOpenTaskId(null);
+  };
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const taskData = await Promise.all(
+        taskIds.map((taskId) =>
+          axios.get(`http://localhost:4000/tasks/${taskId}`)
+        )
+      );
+      const taskList = taskData.map((task, index) => ({
+        id: taskIds[index],
+        taskName: task.data.taskName || "",
+        taskDescription: task.data.taskDescription || "",
+        completed: task.data.completed || false,
+        // image: "",
+        taskPoints: task.data.taskPoints || "",
+        dueDate: "",
+      }));
+      setTasks(taskList);
+      setIsLoaded(true); // update state to indicate API call is complete
     };
+    fetchTasks();
+  }, [taskIds]);
 
-    if (!isLoaded) {
-        return <div>Loading...</div>; // show loading message while API call is in progress
-    }
+  const validationSchema = Yup.object().shape({
+    taskName: Yup.string().required("Required"),
+    taskDescription: Yup.string().required("Required"),
+    taskPoints: Yup.number()
+      .typeError("Must be a number")
+      .positive("Must be positive")
+      .required("Required"),
+    dueDate: Yup.date().required("Required"),
+  });
 
-    return (
-        <div>
-            <h1>Tasks {taskIds.length}</h1>
-            <h4>Total Tasks</h4>
-            {tasks.map((task, index) => (
-                <div key={index}>
-                    <h2>{task.taskName} {task.taskPoints} points Edit</h2>
-                    <Formik
-                        initialValues={task}
-                        validationSchema={validationSchema}
-                        onSubmit={(values) => handleSubmit(values, taskIds[index])}
-                    >
-                        {({ values, errors, touched }) => (
-                            <Form>
-                                <div>
-                                    <label htmlFor="taskName">Task Name</label>
-                                    <Field type="text" name="taskName" />
-                                    <ErrorMessage name="taskName" />
-                                </div>
+  const handleSubmit = (values, taskId) => {
+    axios
+      .put(`http://localhost:4000/tasks/${taskId}`, values)
+      .then(() => {
+        console.log("Task updated successfully");
+        navigate("/tasksrewards");
+      })
+      .catch((err) => console.log(err));
+  };
 
-                                <div>
-                                    <label htmlFor="taskDescription">Task Description</label>
-                                    <Field type="text" name="taskDescription" />
-                                    <ErrorMessage name="taskDescription" />
-                                </div>
+  if (!isLoaded) {
+    return <div>Loading...</div>; // show loading message while API call is in progress
+  }
 
-                                <div>
-                                    <label htmlFor="completed">Completed?</label>
-                                    <Field type="checkbox" name="completed" />
-                                </div>
+  return (
+    <div>
+      <h1>Tasks {taskIds.length}</h1>
+      <h4>Total Tasks</h4>
+      {tasks.map((task, index) => (
+        <div key={index}>
+          <h2>
+            {task.taskName} {task.taskPoints} coins
+          </h2>
+          <h4>Edit</h4>
+            <button onClick={() => handleOpen(index)}>Open Form</button>
+          {openTaskId === index && (
+            <Formik
+              initialValues={task}
+              validationSchema={validationSchema}
+              onSubmit={(values) => handleSubmit(values, taskIds[index])}
+            >
+              {({ values, errors, touched }) => (
+                <Form>
+                  <div>
+                    <label htmlFor="taskName">Task Name</label>
+                    <Field type="text" name="taskName" />
+                    <ErrorMessage name="taskName" />
+                  </div>
 
-                                <div>
+                  <div>
+                    <label htmlFor="taskDescription">Task Description</label>
+                    <Field type="text" name="taskDescription" />
+                    <ErrorMessage name="taskDescription" />
+                  </div>
+
+                  <div>
+                    <label htmlFor="completed">Completed?</label>
+                    <Field type="checkbox" name="completed" />
+                  </div>
+
+                  {/* <div>
                                     <label htmlFor="image">Image URL</label>
                                     <Field type="text" name="image" />
                                     <ErrorMessage name="image" />
-                                </div>
+                                </div> */}
 
-                                <div>
-                                    <label htmlFor="taskPoints">Task Points</label>
-                                    <Field type="number" name="taskPoints" />
-                                    <ErrorMessage name="taskPoints" />
-                                </div>
+                  <div>
+                    <label htmlFor="taskPoints">Task Points</label>
+                    <Field type="number" name="taskPoints" />
+                    <ErrorMessage name="taskPoints" />
+                  </div>
 
-                                <div>
-                                    <label htmlFor="dueDate">Due Date</label>
-                                    <Field type="date" name="dueDate" />
-                                    <ErrorMessage name="dueDate" />
-                                </div>
+                  <div>
+                    <label htmlFor="dueDate">Due Date</label>
+                    <Field type="date" name="dueDate" />
+                    <ErrorMessage name="dueDate" />
+                  </div>
 
-                                <button type="submit">Update Task</button>
-                            </Form>
-                        )}
-                    </Formik>
-                </div>
-            ))}
+                  <button type="submit">Update Task</button>
+                  <button onClick={handleClose}>Close Form</button>
+                </Form>
+              )}
+            </Formik>
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ShowAndEditTask;
--- src/pages/ChildDashboard.jsx
+++ src/pages/ChildDashboard.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ChildDashboardNavBar from '../components/ChildDashboard/ChildDashboardNavBar'
-import QuestsBar from '../components/ChildDashboard/QuestsBar'
-import RedeemRewards from '../components/ChildDashboard/RedeemRewards'
-import LeaderBoard from '../components/ChildDashboard/'
+// import React from 'react'
+// import ChildDashboardNavBar from '../components/ChildDashboard/ChildDashboardNavBar'
+// import QuestsBar from '../components/ChildDashboard/QuestsBar'
+// import RedeemRewards from '../components/ChildDashboard/RedeemRewards'
+// import LeaderBoard from '../components/ChildDashboard'
 
-export default function ChildDashboard() {
-    return (
-        <div>
-            <ChildDashboardNavBar />
-            <LeaderBoard />
-            <QuestsBar />
-            <RedeemRewards />
-        </div>
-    )
-}
+// export default function ChildDashboard() {
+//     return (
+//         <div>
+//             {/* <ChildDashboardNavBar />
+//             <LeaderBoard /> */}
+//             <QuestsBar />
+//             <RedeemRewards />
+//         </div>
+//     )
+// }
