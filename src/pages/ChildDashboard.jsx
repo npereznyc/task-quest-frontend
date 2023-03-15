33ab8605cbd3010c0bef231a15bdae172dbd3019
daryacoding <abdullahzadeh.darya@gmail.com>
2023-03-15 14:03:20 -0400

    fetch api works

--- src/pages/ChildDashboard.jsx
+++ src/pages/ChildDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ChildDashboardNavBar from '../components/ChildDashboard/ChildDashboardNavBar'
 import QuestsBar from '../components/ChildDashboard/QuestsBar'
 import RedeemRewards from '../components/ChildDashboard/RedeemRewards'
-import LeaderBoard from '../components/ChildDashboard/'
+import LeaderBoard from '../components/ChildDashboard/LeaderBoard'
 
 export default function ChildDashboard() {
     return (
