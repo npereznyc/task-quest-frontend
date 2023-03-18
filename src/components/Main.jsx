812f5714886bc4eec6400bf0322e3322457492b5
mtaraq5@gmail.com <mtaraq5@gmail.com>
2023-03-18 10:47:42 -0400

    add useState to registerForm to show that user has registered

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,25 +13,28 @@ import "../style/editTask.css";
 import "../style/quests&rewards.css";
 import "../style/edit&assign.css";
 import "../App.css";
-import '../style/childDashboard.css'
+import "../style/childDashboard.css";
 import QuestsAndRewards from "./CaregiverDashboard/QuestsAndRewards";
 import ChildDashboard from "../pages/ChildDashboard";
 import AddChild from "./CaregiverDashboard/AddChild";
-
 
 const Main = () => {
   return (
     <div>
       <Routes>
-
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<WhoLogsIn />} />
         <Route path="/childlogin" element={<ChildLogin />} />
         <Route path="/childdashboard" element={<ChildDashboard />} />
         <Route path="/caregiverlogin" element={<CaregiverLogin />} />
-        <Route path="register" element={<RegisterForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+
         <Route path="/caregiverdashboard" element={<CaregiverDashboard />} />
-        <Route path="/caregiverdashboard/QuestsAndRewards" element={<QuestsAndRewards />} />
+        <Route
+          path="/caregiverdashboard/QuestsAndRewards"
+          element={<QuestsAndRewards />}
+        />
+
         {/* <Route path="/addChild" element={<AddChild />} /> */}
         <Route path="/tasksrewards" />
       </Routes>
---
+++ src/pages/caregiverRegistered.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/login.css";
+
+function caregiverRegistered() {
+  return (
+    <>
+      <div>WORK</div>
+    </>
+  );
+}
+
+export default caregiverRegistered;
