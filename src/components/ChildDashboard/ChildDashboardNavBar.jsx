35c6cdbe31d561f5fc2b2e7f9a370644d9ee1893
EmilyJarecki <jareckiemily@yahoo.com>
2023-03-19 17:55:52 -0500

    finished styling

--- src/components/ChildDashboard/ChildDashboardNavBar.jsx
+++ src/components/ChildDashboard/ChildDashboardNavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function ChildDashboardNavBar() {
+
     return (
         <>
         <div className="caregiverDashboard-navLogo"></div>
